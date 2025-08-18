92c9cdce607b5b7447037ed611e15ed8827147b0
Karim Yasser <karimmyasserr@gmail.com>
2025-08-18 13:44:18 +0300

    initial commit

---
+++ app/src/main/java/com/example/recipebox/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.example.recipebox.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF4058A0)
+val Secondary = Color(0xFFFF6339)
+val Tertiary = Color(0xFFDEE21B)
+val White = Color(0xFFF6FBF4)
+val LightGrey = Color(0xFFF9F9F9)
+val Grey = Color(0xFFCBCBCB)
+val DarkGrey = Color(0xFF717171)
+val NeutralBlack = Color(0xFF353535)
+val Black = Color(0xFF0E0E0E)
+
+val Error = Color(0xFFF96D63)
+val ErrorLight = Color(0xFFFED4C1)
+val Success = Color(0xFF388916)
+val SuccessLight = Color(0xFFCAF3A1)
+val Warning = Color(0xFFF8A828)
+val WarningLight = Color(0xFFFEE7A8)
+val Info = Color(0xFF05B5BF)
+val InfoLight = Color(0xFF97F8E5)
+
